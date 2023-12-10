8f9e8feff5965b54e69f1738429460a8c8657d81
BYDOC <burakyilmaz01@gmail.com>
2023-12-10 03:43:42 +0300

    Board ready

---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+    //state update fonksiyonunu cağırmak komponentin tekrar cagırılmasına sebep oluyor
+    //farklı component instanceları birbirinden izole calısır. Birindeki state degisimi digerini etkilemez. Player1 componentine tıklanınca state update fonksiyonu calısır 
+    //ve komponent tekrar cagırılır. Ancak bu Player2 componentine etki etmez
+    function handleEditClick() {
+        //state'in degerini, state'in eski degerine bakarak degistirmek istiyorum
+        setIsEditing((editing) => !editing);
+    }
+    function handleChange(event) {
+        console.log(event)
+        setPlayerName(event.target.value);
+    }
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />
+    }
+    return (
+        <li>
+            <span className="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{(isEditing) ? "Save" : "Edit"}</button>
+        </li>
+    );
+}
